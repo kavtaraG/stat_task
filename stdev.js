dc77e0bfacfa62651f126596644a59fbdbf38cb3
kavtaraG <90311433+kavtaraG@users.noreply.github.com>
2022-04-01 05:53:27 -0700

    Update stdev.js

--- stdev.js
+++ stdev.js
@@ -4,10 +4,11 @@ const stat = require('./index');
 const stdDev = avg();
 
 const stVariance = (() => {
+    let n = 3500000;
     let v = stat.reduce((x, y) => x + y);
     
-    let sqrv = stat.reduce((x, y) => x + y)/stat.length;
-    let stDev = Math.sqrt(((Math.pow((v - sqrv), 2))/(stat.length - 1)));
+    let sqrv = stat.reduce((x, y) => x + y)/n;
+    let stDev = Math.sqrt(((Math.pow((v - sqrv), 2))/(n - 1)));
     console.log(`Standard Devation = ${stDev}`);
 });
 stVariance();
