1e2d8e329fe231f33e3a3498c410ce0aa4083ad0
kavtaraG <90311433+kavtaraG@users.noreply.github.com>
2022-04-01 05:52:35 -0700

    Update avg.js

--- avg.js
+++ avg.js
@@ -3,7 +3,8 @@ sumStat= stat.reduce((x, y) => x + y);
 console.log(`sum of array = ${sumStat}`);
 
 const avg = (() => {
-    let average = sumStat/stat.length;
+    let n = 3500000;
+    let average = sumStat/n;
     console.log(`Average = ${average}`);
 });
 avg();
